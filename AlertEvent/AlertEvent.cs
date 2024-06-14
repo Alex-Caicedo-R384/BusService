50ef327db1086c802e9d670acadca7f32ab789cd
Alex_Caicedo_R384 <chevyagcr@gmail.com>
2024-06-14 18:19:03 -0500

    Agregar archivos de proyecto.

---
+++ AlertEvent/AlertEvent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusService.AlertEvent
+{
+    public class AlarmEvent
+    {
+        public string Id { get; set; }
+        public string Message { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
